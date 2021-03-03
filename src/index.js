d1770cd27d54bfc50337b8f9151df376b270a018
AlexeyS <xengawe@gmail.com>
2021-03-02 19:57:36 -0800

    do some code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import './styles/main.css';
 // import './styles/index.scss';
+import * as d3 from 'd3';
 
-alert('connected!')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  continueButton()
+})
+
+function continueButton(){
+  
+  const main = document.getElementsByClassName('main-div')
+  main[0].classList.add('hidden')
+
+
+  const intro = document.getElementsByClassName('intro')
+  intro[0].classList.remove('hidden')
+  // debugger
+  let button = intro[0].lastElementChild
+  button.addEventListener('click', () => {
+    main[0].classList.remove('hidden')
+    intro[0].classList.add('hidden')
+  })
+
+}

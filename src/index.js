1edbfced4640aea8b23a0188f1125c44df3b84c7
AlexeyS <xengawe@gmail.com>
2021-03-02 21:38:31 -0800

    do intro page

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ function continueButton(){
   
   const main = document.getElementsByClassName('main-div')
   main[0].classList.add('hidden')
-
-
+  // d3.select("li").style("color", "green"); test
+ 
   const intro = document.getElementsByClassName('intro')
   intro[0].classList.remove('hidden')
   // debugger
@@ -23,6 +23,7 @@ function continueButton(){
   button.addEventListener('click', () => {
     main[0].classList.remove('hidden')
     intro[0].classList.add('hidden')
+    
   })
 
 }

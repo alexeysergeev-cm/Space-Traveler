679b65ed30e364c4bc47bb6ece876680ba189b8a
AlexeyS <xengawe@gmail.com>
2021-03-03 22:06:53 -0800

    add some stars

--- src/index.js
+++ src/index.js
@@ -36,40 +36,33 @@ function continueButton(){
     main[0].classList.remove('hidden')
     intro[0].classList.add('hidden')
     
-    update()
-    loadData()
+    mainPageTransition()
+    loadDefaultData()
   })
 }
 
-
-
-function update() {
+function mainPageTransition() {
   d3.select('rect').transition()
     .ease(d3.easeLinear)
     .duration(3000)
     .attr("fill", "blue")
 }
 
-async function loadData(){
+async function loadDefaultData(){
   // let arr = []
   // d3.csv('/data/exoplanetsFiltered.csv', function(data){
   //   console.log(data)
   // })
-  let arr = await d3.csv('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?&table=exoplanets&where=st_dist<5') //, function(data){
-    // console.log(data)  
-
-  // })
-  debugger
+  let arr = await d3.csv('https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?&table=exoplanets&where=st_dist<5') // default nasa api
+  // let arr = await d3.csv('https://api.le-systeme-solaire.net/rest/bodies/') // solar system api
+  
+  // debugger
   d3.select("body")
     .selectAll("p")
     .data(arr)
     .enter().append("p")
     .text(function(d) { return "I’m number " + d.st_dist + "!"; });
-  // d3.select("body")
-  // .selectAll("p")
-  // .data([4, 8, 15, 16, 23, 42])
-  // .enter().append("p")
-  //   .text(function(d) { return "I’m number " + d + "!"; });
+ 
 }
 
 // test , it works insie of contBTN()

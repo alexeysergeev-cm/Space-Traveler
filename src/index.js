fb03ab60cda26589aab0191afeb7ac78629df6f3
AlexeyS <xengawe@gmail.com>
2021-03-05 10:11:00 -0800

    add human data

--- src/index.js
+++ src/index.js
@@ -66,7 +66,6 @@ st_raderr2: "-0.02",st_radlim: "0",st_radn: "2",st_raerr: "0.000004",st_teff: "3
     .enter().append("p")
     .text(function(d) { return d.pl_name });
 
-    d = []
 
   //all btns
   d3.select(".left-switch")
@@ -85,7 +84,16 @@ st_raderr2: "-0.02",st_radlim: "0",st_radn: "2",st_raerr: "0.000004",st_teff: "3
     .style('background-color', 'rgb(90 250 13)')
      
   //store data from api request
-  let data;
+  // let data;
+  let data = [{dec: "-62.679485", dec_str: "-62d40m46.1s", gaia_gmag: "8.954", gaia_gmagerr: "", gaia_gmaglim: "0", pl_bmassj: "0.00400", pl_bmassjerr1: "0.00060",
+  pl_bmassjerr2: "-0.00053",pl_bmassjlim: "0",pl_bmassn: "1",pl_bmassprov: "Msini",pl_controvflag: "0",pl_dens: "",pl_denserr1: "",pl_denserr2: "",pl_denslim: "",
+pl_densn: "0",pl_discmethod: "Radial Velocity",pl_facility: "European Southern Observatory",pl_hostname: "Proxima Cen",pl_k2flag: "0",pl_kepflag: "0",pl_letter: "b",
+pl_name: "Proxima Cen b",pl_nnotes: "0",pl_orbeccen: "0.350000",pl_orbeccenerr1: "",pl_orbeccenerr2: "",pl_orbeccenlim: "1",pl_orbeccenn: "2",pl_orbincl: "",pl_orbinclerr1: "", 
+pl_orbinclerr2: "",pl_orbincllim: "",pl_orbincln: "0",pl_orbper: "11.18600000",pl_orbpererr1: "0.00100000",pl_orbpererr2: "-0.00200000",pl_orbperlim: "0",pl_orbpern: "2",pl_orbsmax: "0.048500",
+pl_orbsmaxerr1: "0.004100",pl_orbsmaxerr2: "-0.005100",pl_orbsmaxlim: "0",pl_orbsmaxn: "1",pl_pnum: "1",pl_radj: "",pl_radjerr1: "",pl_radjerr2: "",pl_radjlim: "",pl_radn: "0",pl_ttvflag: "0",
+ra: "217.428955",ra_str: "14h29m42.95s",rowupdate: "2016-08-25",st_decerr: "0.000004",st_dist: "1.30",st_disterr1: "0.00",st_disterr2: "-0.00",st_distlim: "0",st_distn: "3",st_mass: "0.12",st_masserr1: "0.01",
+st_masserr2: "-0.01",st_masslim: "0",st_massn: "2",st_optband: "V (Johnson)",st_optmag: "11.110",st_optmagerr: "",st_optmaglim: "0",st_posn: "4",st_rad: "0.14",st_raderr1: "0.02",
+st_raderr2: "-0.02",st_radlim: "0",st_radn: "2",st_raerr: "0.000004",st_teff: "3050.00",st_tefferr1: "100.00",st_tefferr2: "-100.00",st_tefflim: "0",st_teffn: "2"}]
 
   //selecting distance
   d3.selectAll('button')
@@ -157,12 +165,25 @@ function showPlanetStats(planet){
   d3.select(".planet-data")
     .selectAll("svg").remove()
 
-  let data = [planet.st_dist, planet.pl_pnum, planet.pl_orbper]
+  let LightYearsInOneParsec = 3.26;
+
+  let data = [planet.st_dist * LightYearsInOneParsec, planet.pl_pnum, planet.pl_orbper]
 
   let width = 500
   let height = 350
   let scaleFactor = 10
-  let barHeight = 20;
+  let barHeight = 40;
+
+  let lightYearDistInMiles = 6000000000000; //miles
+  let speedOfLight = 671000000; //mph
+  let voyagerSpeed = 38000; //mph
+  let day = 24; //hrs
+  let yearLength = 365; //days
+
+  let totalMilesToDestination = lightYearDistInMiles * data[0]
+  let yearsToReach = totalMilesToDestination / (voyagerSpeed * day * yearLength)
+  let humanGenerations = yearsToReach / 30
+  let humanData = [yearsToReach, humanGenerations]
 
   let graph = d3.select(".planet-data")
                 .append("svg")
@@ -178,6 +199,32 @@ function showPlanetStats(planet){
                   });
   
   bar.append("rect")
+      .attr("width", function(d) {
+              return d * scaleFactor;
+      })
+      .attr("height", barHeight - 5)
+      .attr("fill", 'red')
+
+  //human-related data
+  d3.select(".other-data")
+    .selectAll("svg").remove()
+
+  let otherWidth = 500;
+  let otherHeight = 290;
+  let graph2 = d3.select(".other-data")
+                .append("svg")
+                .attr("width", otherWidth)
+                .attr("height", otherHeight);
+
+  let bar2 = graph2.selectAll("g")
+                  .data(humanData)
+                  .enter()
+                  .append("g")
+                  .attr("transform", function(d, i) {
+                        return "translate(0," + i * barHeight + ")";
+                  });
+  
+  bar2.append("rect")
       .attr("width", function(d) {
               return d * scaleFactor;
       })
